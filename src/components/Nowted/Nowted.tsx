4f5e0f232a021b2293dfe3dd6cf562e02d82810c
Rizki Maulana Citra <rmaulana.citra@gmail.com>
2023-02-19 16:41:40 +0700

    feat: add placeholder

--- src/components/Nowted/Nowted.tsx
+++ src/components/Nowted/Nowted.tsx
@@ -1,14 +1,31 @@
+import { useActiveFolder } from '@/hooks/useActiveFolder'
 import { useActiveNote } from '@/hooks/useActiveNote'
 import { useNote } from '@/hooks/useNote'
 import { clsxm } from '@/util/clsxm'
+import { HiOutlineDocumentText } from 'react-icons/hi'
 import { NowTedBody } from './NowtedBody'
 import { NowtedHeader } from './NowtedHeader'
 
 export const Nowted = () => {
+  const { folderId } = useActiveFolder()
   const { noteId } = useActiveNote()
   const note = useNote(noteId)
 
-  if (!noteId || !note) return null
+  if (!noteId || !note)
+    return (
+      <div
+        className={clsxm(
+          'w-[calc(100vw-300px)] h-screen',
+          'flex flex-col items-center justify-center',
+          folderId && 'w-[calc(100vw-650px)] bg-accent-1'
+        )}
+      >
+        <HiOutlineDocumentText className='w-20 h-20' />
+        <p className='font-semibold text-[28px]'>
+          {folderId ? 'Select a note to view' : 'Select a folder to start'}
+        </p>
+      </div>
+    )
 
   return (
     <div
