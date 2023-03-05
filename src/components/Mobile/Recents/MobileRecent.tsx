fd23719acef9c8aaddc043e9537a7f14a39b0d3b
Rizki Maulana Citra <rmaulana.citra@gmail.com>
2023-03-05 17:46:05 +0700

    fix: add bg color to hide overflowed item

--- src/components/Mobile/Recents/MobileRecent.tsx
+++ src/components/Mobile/Recents/MobileRecent.tsx
@@ -6,7 +6,7 @@ export const MobileRecents = () => {
   const { recents } = useRecents()
 
   return (
-    <div className={clsxm('flex items-center', 'h-10')}>
+    <div className={clsxm('flex items-center', 'h-10', 'bg-accent-1')}>
       <div className='flex items-center'>
         {recents.map((noteId) => {
           return <MobileRecentItem key={noteId} noteId={noteId} />
